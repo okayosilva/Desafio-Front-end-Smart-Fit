04430a9e0cbc6769ebd43ca90ee0f08f0f20ef10
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-12 10:48:58 -0300

    :sparkles: feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import Logo from '../assets/images/svg/logo.svg'
 
 export function Header() {
   return (
-    <header className="bg-black">
-      <img src={Logo} alt="" />
+    <header className="flex min-h-36 bg-black py-8">
+      <img src={Logo} alt="Smart Fit" className="mx-auto h-20 md:h-28" />
     </header>
   )
 }
