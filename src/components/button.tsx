fc17f2970594da1cd95c4f84bfb2bd0f93b134c2
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-12 12:47:12 -0300

    :sparkles: feat: button

---
+++ src/components/button.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const button = tv({
+  base: [
+    'flex w-full max-w-64 items-center justify-center',
+    'rounded-sm py-2 font-bold',
+    'transition-all duration-300 hover:shadow-md',
+  ],
+  variants: {
+    variant: {
+      primary: 'bg-light-yellow ',
+      secondary: 'border border-light-grey shadow-sm',
+    },
+  },
+  defaultVariants: {
+    variant: 'secondary',
+  },
+})
+
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({ variant, className, ...rest }: ButtonProps) {
+  return <button className={button({ variant, className })} {...rest} />
+}
